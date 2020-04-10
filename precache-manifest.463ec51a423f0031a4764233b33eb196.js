ae64991feb5d5992a64cf3b79e65633de93cd9c1
Dat Bug <dat.t.nguyen@vntrip.vn>
2020-04-10 11:38:23 +0700

    Updates

---
+++ precache-manifest.463ec51a423f0031a4764233b33eb196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d422cef077dca78b1334dd1527d8de44",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "ad5d66d34528a0e64b5b",
+    "url": "/react-cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67132dd07190ffa11e87",
+    "url": "/react-cv/static/js/2.feb77b14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cv/static/js/2.feb77b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5d66d34528a0e64b5b",
+    "url": "/react-cv/static/js/main.3964e26c.chunk.js"
+  },
+  {
+    "revision": "825ffc1cca0003c34b8f",
+    "url": "/react-cv/static/js/runtime-main.3c3df479.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
